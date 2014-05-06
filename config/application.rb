8e75fc1c818ffe7adfc2c3dcce40ca981e92a40a
= <thomas07@vt.edu>
2014-05-06 10:16:32 -0700

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,42 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Wurkforce
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
+
+    # asset precompile setting for Heroku
+    config.assets.initialize_on_precompile = false
+
+    # get sass to compile my files in vendor
+    config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
+
+    # rspec generators
+    config.generators do |g|
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            controller_specs: false,
+            request_specs: true
+        g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wurkforce::Application.config.secret_key_base = '577073f5405896d39856dd01e4bbe6e5edebb9135a7fd2575c6a1e9b13cdfbc84eb166fbb5a77c793e64381edfc1a6918d27dfb2a35bcf1fe5c8349fe50c7feb'
